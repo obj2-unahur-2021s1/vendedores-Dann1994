c156d8ae9840d3291c1317c3f5a04351e02d61c2
Dann1994 <dani_jdv@live.com.ar>
2021-04-05 22:11:28 -0300

    primera

---
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/CentroDeDistribucion.kt
@@ -0,0 +1,20 @@
+package ar.edu.unahur.obj2.vendedores
+
+class CentroDeDistribucion ( val ciudad: Ciudad ) {
+    val vendedores = mutableListOf<Vendedor>()  //Preguntar por lista de varios objetos de distintos tipos
+
+    fun contratarVendedor(unVendedor: Vendedor) {
+        check (vendedores.contains(unVendedor)) {
+            //El vendedor ya está contratado.
+        }
+        vendedores.add(unVendedor)
+    }
+
+    fun vendedorEstrella() = vendedores.maxBy { it.puntajeCertificaciones() }
+
+    fun puedeCubrir(unaCiudad: Ciudad) = vendedores.any {it.puedeTrabajarEn(unaCiudad)}
+
+    fun vendedoresGenericos() = vendedores.filter { it.esGenerico() }
+
+    fun esRobusto() = vendedores.count { it.esFirme() } >= 3
+}
--- src/main/kotlin/ar/edu/unahur/obj2/vendedores/Ciudad.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/vendedores/Ciudad.kt
@@ -3,3 +3,7 @@ package ar.edu.unahur.obj2.vendedores
 class Ciudad(val provincia: Provincia)
 class Provincia(val poblacion: Int)
 
+
+
+
+
